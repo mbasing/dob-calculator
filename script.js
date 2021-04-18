c118dd5838171007a3285b16b54ad948b4146992
Moses Basing <mosesbasing@gmail.com>
2021-04-18 22:10:24 +0000

    Add date of birth and date at the date of filters

--- script.js
+++ script.js
@@ -25,7 +25,44 @@ findYear.addEventListener('click',()=>{
 // Computes the age differerence between two birth dates
 // ==================================================================
 
-let birthDate = document.querySelector('#birthDate').value;
-let ageAtDateOfBirth =document.querySelector('#ageAtDateOfBirth').value;
-let calculateDateDifference = document.querySelector('#calculate');
+const yearSubmit = document.querySelector('#calculate');
+
+let displayYearsBetween=()=>{
+    let dateVal1 = document.querySelector('#birthDate').value;
+    let dateVal2 = document.querySelector('#ageAtDateOfBirth').value;
+
+    let date_diff = (date1, date2)=>{
+        dt1 = new Date(date1);
+        dt2 = new Date(date2);
+
+        return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+    }
+
+    let dateInDays = date_diff(dateVal1, dateVal2);
+    let yearsOld = Math.round(dateInDays/365);
+    let monthsOld = dateInDays % 365;
+
+    let result = `You are ${yearsOld} years and ${monthsOld} months old`;
+    let trueAge = document.querySelector('#age__result');
+
+    if(dateVal1 !== '' && dateVal2 !== ''){
+        trueAge.innerHTML = result;
+    }
+
+    if(dateVal1 > dateVal2){
+        trueAge.innerHTML = 'Enter a valid age range';
+    }
+}
+
+yearSubmit.addEventListener('click', displayYearsBetween);
+
+
+
+
+
+
+
+
+
+
 
